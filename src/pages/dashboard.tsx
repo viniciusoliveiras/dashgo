80a02ce04a1004b6f063dc084613cb436ad72cfc
viniciusoliveiras <vinitag190@gmail.com>
2021-04-01 18:28:51 -0300

    Responsiveness | Responsive dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -76,7 +76,7 @@ export default function Dashboard() {
           <Sidebar />
 
           <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
-            <Box p='8' bgColor='gray.800' borderRadius='8' pb='4'>
+            <Box p={['6', '8']} bgColor='gray.800' borderRadius='8' pb='4'>
               <Text fontSize='lg' mb='4'>
                 Inscritos da semana
                 <Chart
