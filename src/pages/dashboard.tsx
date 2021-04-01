bf9a9916fa142565ca7ada8d14f3af525eb3a81f
viniciusoliveiras <vinitag190@gmail.com>
2021-03-31 21:03:57 -0300

    Loading and dashboard | Page: Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,112 @@
-import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 
-export default function Dashboard() {
-  return (
-    <Flex direction='column' h='100vh'>
-      <Header />
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
 
-      <Flex w='100%' mx='auto' my='6' maxWidth={1480} px='6'>
-        <Sidebar />
+export default function Dashboard() {
+  const options = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[500],
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      type: 'datetime',
+      axisBorder: {
+        color: theme.colors.gray[600],
+      },
+      axisTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: [
+        '2021-03-25T00:00:00.000Z',
+        '2021-03-26T00:00:00.000Z',
+        '2021-03-27T00:00:00.000Z',
+        '2021-03-28T00:00:00.000Z',
+        '2021-03-29T00:00:00.000Z',
+        '2021-03-30T00:00:00.000Z',
+        '2021-03-31T00:00:00.000Z',
+      ],
+    },
+    fill: {
+      opacity: 0.3,
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  };
+
+  const series = [
+    {
+      name: 'seriesOne',
+      data: [30, 70, 230, 61, 20, 150, 66],
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>dashboard | dashgo</title>
+      </Head>
+      <Flex direction='column' h='100vh'>
+        <Header />
+
+        <Flex w='100%' mx='auto' my='6' maxWidth={1480} px='6'>
+          <Sidebar />
+
+          <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+            <Box p='8' bgColor='gray.800' borderRadius='8' pb='4'>
+              <Text fontSize='lg' mb='4'>
+                Inscritos da semana
+                <Chart
+                  options={options}
+                  series={series}
+                  type='area'
+                  height={160}
+                />
+              </Text>
+            </Box>
+
+            <Box
+              p='8'
+              bgColor='gray.800'
+              borderRadius='8'
+              // pb='4'
+            >
+              <Text fontSize='lg' mb='4'>
+                Texa de abertura
+                <Chart
+                  options={options}
+                  series={series}
+                  type='area'
+                  height={160}
+                />
+              </Text>
+            </Box>
+          </SimpleGrid>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,33 @@
-import { Flex, Button, Stack } from '@chakra-ui/react';
+import Head from 'next/head';
 import { Input } from '../components/Form/input';
+import { Flex, Button, Stack } from '@chakra-ui/react';
 
 export default function SignIn() {
   return (
-    <Flex w='100vw' h='100vh' align='center' justify='center'>
-      <Flex
-        as='form'
-        w='100%'
-        maxWidth={360}
-        bg='gray.800'
-        p='8'
-        borderRadius='8'
-        flexDirection='column'
-      >
-        <Stack spacing='4'>
-          <Input name='email' type='email' label='E-mail' />
-          <Input name='password' type='password' label='Senha' />
-        </Stack>
+    <>
+      <Head>
+        <title>sign in | dashgo</title>
+      </Head>
+      <Flex w='100vw' h='100vh' align='center' justify='center'>
+        <Flex
+          as='form'
+          w='100%'
+          maxWidth={360}
+          bg='gray.800'
+          p='8'
+          borderRadius='8'
+          flexDirection='column'
+        >
+          <Stack spacing='4'>
+            <Input name='email' type='email' label='E-mail' />
+            <Input name='password' type='password' label='Senha' />
+          </Stack>
 
-        <Button type='submit' mt='6' colorScheme='pink' size='lg'>
-          Entrar
-        </Button>
+          <Button type='submit' mt='6' colorScheme='pink' size='lg'>
+            Entrar
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
