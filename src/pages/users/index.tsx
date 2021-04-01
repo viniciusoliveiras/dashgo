c0e1a4e801c94414568903889f5c01e5290f677c
viniciusoliveiras <vinitag190@gmail.com>
2021-04-01 15:51:04 -0300

    User control | Page: User list

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,139 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w='100%' mx='auto' my='6' maxWidth={1480} px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+            </Heading>
+            <Button
+              as='a'
+              size='sm'
+              fontSize='sm'
+              colorScheme='pink'
+              leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme='whiteAlpha'>
+            <Thead>
+              <Tr>
+                <Th px='6' color='gray.300' w='8'>
+                  <Checkbox colorScheme='pink'></Checkbox>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w='8'></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px='6'>
+                  <Checkbox colorScheme='pink'></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      vinitag190@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>01 abril 2021</Td>
+                <Td>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='purple'
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px='6'>
+                  <Checkbox colorScheme='pink'></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      vinitag190@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>01 abril 2021</Td>
+                <Td>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='purple'
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px='6'>
+                  <Checkbox colorScheme='pink'></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      vinitag190@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>01 abril 2021</Td>
+                <Td>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='purple'
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
