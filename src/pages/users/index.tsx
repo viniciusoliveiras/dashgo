1c998e4954cd5dcdc6070e980d7e77438ff1642d
viniciusoliveiras <vinitag190@gmail.com>
2021-04-11 14:11:12 -0300

    React Query concepts | Configuring DevTools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,25 +24,31 @@ import Link from 'next/link';
 import { useQuery } from 'react-query';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users');
+      const data = await response.json();
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      };
-    });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -80,7 +86,7 @@ export default function UserList() {
 
             {isLoading ? (
               <Flex justify='center'>
-                <Spinner />
+                <Spinner mt='14' />
               </Flex>
             ) : error ? (
               <Flex justify='center'>
