507c4af3c6dda37e704e69291cdb3ee7b9e51d43
viniciusoliveiras <vinitag190@gmail.com>
2021-04-11 13:32:13 -0300

    React Query concepts | Configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,6 +6,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -15,24 +16,25 @@ import {
   Tr,
   useBreakpointValue,
 } from '@chakra-ui/react';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header/index';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
 
   return (
     <>
@@ -63,65 +65,77 @@ export default function UserList() {
               </Link>
             </Flex>
 
-            <Table colorScheme='whiteAlpha'>
-              <Thead>
-                <Tr>
-                  <Th px={['4', '4', '6']} color='gray.300' w='8'>
-                    <Checkbox colorScheme='pink'></Checkbox>
-                  </Th>
-                  <Th>Usuário</Th>
-                  {isWideVersion && <Th>Data de cadastro</Th>}
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px={['4', '4', '6']}>
-                    <Checkbox colorScheme='pink'></Checkbox>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight='bold'>Vinícius Oliveira</Text>
-                      <Text fontSize='sm' color='gray.300'>
-                        vinitag190@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>01 abril 2021</Td>}
-                </Tr>
+            {isLoading ? (
+              <Flex justify='center'>
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justify='center'>
+                <Text>Falha ao obter dados dos usuários</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme='whiteAlpha'>
+                  <Thead>
+                    <Tr>
+                      <Th px={['4', '4', '6']} color='gray.300' w='8'>
+                        <Checkbox colorScheme='pink'></Checkbox>
+                      </Th>
+                      <Th>Usuário</Th>
+                      {isWideVersion && <Th>Data de cadastro</Th>}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink'></Checkbox>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            vinitag190@gmail.com
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>01 abril 2021</Td>}
+                    </Tr>
 
-                <Tr>
-                  <Td px={['4', '4', '6']}>
-                    <Checkbox colorScheme='pink'></Checkbox>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight='bold'>Vinícius Oliveira</Text>
-                      <Text fontSize='sm' color='gray.300'>
-                        vinitag190@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>01 abril 2021</Td>}
-                </Tr>
+                    <Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink'></Checkbox>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            vinitag190@gmail.com
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>01 abril 2021</Td>}
+                    </Tr>
 
-                <Tr>
-                  <Td px={['4', '4', '6']}>
-                    <Checkbox colorScheme='pink'></Checkbox>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight='bold'>Vinícius Oliveira</Text>
-                      <Text fontSize='sm' color='gray.300'>
-                        vinitag190@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>01 abril 2021</Td>}
-                </Tr>
-              </Tbody>
-            </Table>
+                    <Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink'></Checkbox>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>Vinícius Oliveira</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            vinitag190@gmail.com
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>01 abril 2021</Td>}
+                    </Tr>
+                  </Tbody>
+                </Table>
 
-            <Pagination />
+                <Pagination />
+              </>
+            )}
           </Box>
         </Flex>
       </Box>
