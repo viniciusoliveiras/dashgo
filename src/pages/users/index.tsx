3c6e3475219f2a2da9549cef1dd87d3dcef7f091
viniciusoliveiras <vinitag190@gmail.com>
2021-04-12 10:26:03 -0300

    Building application | Signaling data refetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import Link from 'next/link';
 import { useQuery } from 'react-query';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     'users',
     async () => {
       const response = await fetch('http://localhost:3000/api/users');
@@ -70,6 +70,9 @@ export default function UserList() {
             <Flex mb='8' justify='space-between' align='center'>
               <Heading size='lg' fontWeight='normal'>
                 Usuários
+                {!isLoading && isFetching && (
+                  <Spinner size='sm' color='gray.500' ml='4' />
+                )}
               </Heading>
               <Link href='/users/create' passHref>
                 <Button
